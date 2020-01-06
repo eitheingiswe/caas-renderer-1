330fca1d3ff9f11b50d1e91ca56aca1c6fd17ce9
Sim Boon Long <simboonlong@gmail.com>
2020-01-06 16:13:38 +0800

    fix(card positioning): center scene with abit margin-top instead of dead center (#2)

--- src/templates/customTemplate/customTemplate.tsx
+++ src/templates/customTemplate/customTemplate.tsx
@@ -126,9 +126,8 @@ const LicenseRatings = styled.div`
 
 const Scene = styled.div`
   position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+  width: 100%;
+  margin-top: 2rem;
   perspective: 1000;
 `;
 
